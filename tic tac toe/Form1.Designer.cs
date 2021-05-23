f268ca9d029adfa0b165318ca1e413f7595839a5
jhandles7 <77910911+jhandles7@users.noreply.github.com>
2021-05-23 21:26:42 +0800

    Declare Variable and test code for tile1 and tile2 for X and O

--- tic tac toe/Form1.Designer.cs
+++ tic tac toe/Form1.Designer.cs
@@ -43,79 +43,89 @@ namespace tic_tac_toe
             // 
             // tile1
             // 
-            this.tile1.Location = new System.Drawing.Point(0, 20);
+            this.tile1.Font = new System.Drawing.Font("Wide Latin", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.tile1.Location = new System.Drawing.Point(12, 95);
             this.tile1.Name = "tile1";
             this.tile1.Size = new System.Drawing.Size(91, 76);
             this.tile1.TabIndex = 0;
             this.tile1.UseVisualStyleBackColor = true;
+            this.tile1.Click += new System.EventHandler(this.tile1_Click);
             // 
             // tile2
             // 
-            this.tile2.Location = new System.Drawing.Point(97, 20);
+            this.tile2.Location = new System.Drawing.Point(118, 94);
             this.tile2.Name = "tile2";
             this.tile2.Size = new System.Drawing.Size(91, 76);
             this.tile2.TabIndex = 1;
             this.tile2.UseVisualStyleBackColor = true;
+            this.tile2.Click += new System.EventHandler(this.tile2_Click);
             // 
             // tile3
             // 
-            this.tile3.Location = new System.Drawing.Point(194, 20);
+            this.tile3.Location = new System.Drawing.Point(225, 94);
             this.tile3.Name = "tile3";
             this.tile3.Size = new System.Drawing.Size(91, 76);
             this.tile3.TabIndex = 2;
             this.tile3.UseVisualStyleBackColor = true;
+            this.tile3.Click += new System.EventHandler(this.tile3_Click);
             // 
             // tile4
             // 
-            this.tile4.Location = new System.Drawing.Point(0, 102);
+            this.tile4.Location = new System.Drawing.Point(12, 177);
             this.tile4.Name = "tile4";
             this.tile4.Size = new System.Drawing.Size(91, 76);
             this.tile4.TabIndex = 3;
             this.tile4.UseVisualStyleBackColor = true;
+            this.tile4.Click += new System.EventHandler(this.tile4_Click);
             // 
             // tile5
             // 
-            this.tile5.Location = new System.Drawing.Point(97, 102);
+            this.tile5.Location = new System.Drawing.Point(118, 177);
             this.tile5.Name = "tile5";
             this.tile5.Size = new System.Drawing.Size(91, 76);
             this.tile5.TabIndex = 4;
             this.tile5.UseVisualStyleBackColor = true;
+            this.tile5.Click += new System.EventHandler(this.tile5_Click);
             // 
             // tile6
             // 
-            this.tile6.Location = new System.Drawing.Point(194, 102);
+            this.tile6.Location = new System.Drawing.Point(225, 177);
             this.tile6.Name = "tile6";
             this.tile6.Size = new System.Drawing.Size(91, 76);
             this.tile6.TabIndex = 5;
             this.tile6.UseVisualStyleBackColor = true;
+            this.tile6.Click += new System.EventHandler(this.tile6_Click);
             // 
             // tile8
             // 
-            this.tile8.Location = new System.Drawing.Point(97, 184);
+            this.tile8.Location = new System.Drawing.Point(118, 258);
             this.tile8.Name = "tile8";
             this.tile8.Size = new System.Drawing.Size(91, 76);
             this.tile8.TabIndex = 7;
             this.tile8.UseVisualStyleBackColor = true;
+            this.tile8.Click += new System.EventHandler(this.tile8_Click);
             // 
             // tile7
             // 
-            this.tile7.Location = new System.Drawing.Point(0, 184);
+            this.tile7.Location = new System.Drawing.Point(12, 259);
             this.tile7.Name = "tile7";
             this.tile7.Size = new System.Drawing.Size(91, 76);
             this.tile7.TabIndex = 8;
             this.tile7.UseVisualStyleBackColor = true;
+            this.tile7.Click += new System.EventHandler(this.tile7_Click);
             // 
             // tile9
             // 
-            this.tile9.Location = new System.Drawing.Point(194, 184);
+            this.tile9.Location = new System.Drawing.Point(225, 259);
             this.tile9.Name = "tile9";
             this.tile9.Size = new System.Drawing.Size(91, 76);
             this.tile9.TabIndex = 9;
             this.tile9.UseVisualStyleBackColor = true;
+            this.tile9.Click += new System.EventHandler(this.tile9_Click);
             // 
             // btn_play_again
             // 
-            this.btn_play_again.Location = new System.Drawing.Point(97, 271);
+            this.btn_play_again.Location = new System.Drawing.Point(140, 340);
             this.btn_play_again.Name = "btn_play_again";
             this.btn_play_again.Size = new System.Drawing.Size(83, 29);
             this.btn_play_again.TabIndex = 10;
@@ -127,7 +137,7 @@ namespace tic_tac_toe
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.ClientSize = new System.Drawing.Size(286, 312);
+            this.ClientSize = new System.Drawing.Size(331, 381);
             this.Controls.Add(this.btn_play_again);
             this.Controls.Add(this.tile9);
             this.Controls.Add(this.tile7);
--- tic tac toe/Form1.cs
+++ tic tac toe/Form1.cs
@@ -23,5 +23,77 @@ namespace tic_tac_toe
         }
 
         
+        int player_turn = 0;
+
+        private void tile1_Click(object sender, EventArgs e)
+        {
+            if (player_turn %2 == 0)
+            {
+                
+                tile1.Text = "X";
+                player_turn++;
+                
+            }
+            else
+            {
+                
+                tile1.Text = "O";
+                player_turn++;
+                
+            }
+        }
+
+        private void tile2_Click(object sender, EventArgs e)
+        {
+            if (player_turn % 2 == 0)
+            {
+                
+                tile2.Text = "X";
+                player_turn++;
+                
+            }
+            else
+            {
+                
+                tile2.Text = "O";
+                player_turn++;
+                
+            }
+        }
+
+        private void tile3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tile4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tile5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tile6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tile7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tile8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tile9_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
